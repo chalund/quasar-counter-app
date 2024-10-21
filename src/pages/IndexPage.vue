df93cea311528d62547715808ae5aaa9f9887d5d
Charlotte Lund <chalund@gmail.com>
2024-10-21 11:29:22 +0200

    adds buttons, input-field, background

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,15 +1,25 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+  <q-page class="flex flex-center text-white">
+    <div class="row">
+     <q-input
+     input-class="text-center text-h5 text-white"
+     color="teal"
+        filled
+        placeholder="Counter" />
+    </div>
+    <div class="row full-width items-center">
+      <div class="col text-center">
+        <q-btn round icon="remove" size="xl" />
+      </div>
+      <div class="col text-center text-h2">23</div>
+      <div class="col text-center">
+        <q-btn round icon="add" size="xl" />
+      </div>
+    </div>
+    <div class="row">
+      <q-btn round icon="restart_alt" size="xl" />
+    </div>
   </q-page>
 </template>
 
-<script setup>
-defineOptions({
-  name: 'IndexPage'
-});
-</script>
+<script setup></script>
